d48ebbc718572cd96fd5f80233329bc917a47c6f
B.CSV <chandhrasai.14@gmail.com>
2025-03-19 10:11:53 +0000

    Added updates to script.jsl

--- chat important/script.js
+++ chat important/script.js
@@ -2,7 +2,7 @@ function checkPassword() {
     var password = document.getElementById("password").value;
 
     // Type your password here (e.g., 'mySecurePassword')
-    if (password === '7341926851') {
+    if (password === 'lucky') {
         window.location.href = "home.html";
     } else {
         alert("Incorrect password. Please try again.");
